a4fb077e677ffe2c784b62872ae1837a2e4d66d5
affandi <mursitaffandi@gmail.com>
2016-12-25 11:07:09 +0700

    balksfbsjbfw

---
+++ app/src/main/java/id/co/imastudio/uploadmultyparttype/ConfigUrl.java
@@ -0,0 +1,13 @@
+package id.co.imastudio.uploadmultyparttype;
+
+/**
+ * Created by affandi on 11/14/16.
+ */
+
+public class ConfigUrl {
+    // seting API to upload multypart file
+    public static final String FILE_UPLOAD_URL = "http://192.168.10.104/AndroidFileUpload/fileUpload.php";
+
+    // Setting directory name to store captured image and video
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
